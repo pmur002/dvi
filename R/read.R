a84f88d71e2e87a599985b49c5e710cf107053da
Paul Murrell <paul@stat.auckland.ac.nz>
2024-10-14 12:59:20 +1300

    calve off {dvi} from {xdvir}

---
+++ R/read.R
@@ -0,0 +1,22 @@
+
+readDVI <- function(f) {
+    ## Create a list that is known to be too long
+    ops <- vector("list", file.size(f))
+    done <- FALSE
+    offset <- 0
+    length <- 1
+    while (!done) {
+        op <- readFormat(f, opFormat, offset=offset)
+        if (blockValue(op$blocks$op.opcode) == 249) {
+            done <- TRUE
+        }
+        offset <- offset + op$nbytes
+        ops[[length]] <- op
+        length <- length + 1
+    }
+    result <- ops[1:(length - 1)]
+    class(result) <- "DVI"
+    result
+}
+
+
---
+++ tests/dvi.R
@@ -0,0 +1,10 @@
+
+library(dvi)
+
+readDVI(system.file("DVI", "test-pdf.dvi", package="dvi"))
+
+readDVI(system.file("DVI", "test-lua.dvi", package="dvi"))
+
+readDVI(system.file("DVI", "test-xe.xdv", package="dvi"))
+
+readDVI(system.file("DVI", "test-up.dvi", package="dvi"))
