6a47ddd42d3db9c8c560e3ff15c75e5aea7a0890
Paul Murrell <paul@stat.auckland.ac.nz>
2024-10-30 20:23:19 +1300

    add opNames()

--- R/read.R
+++ R/read.R
@@ -1,28 +1,4 @@
 
-opCodes <- function(dvi) {
-    sapply(dvi,
-           function(op) {
-               blockValue(op$blocks$op.opcode)
-           })
-}
-
-opParams <- function(dvi) {
-    lapply(dvi,
-           function(op) {
-               ## Remove code (first component of blocks)
-               nocode <- op$blocks[-1]
-               params <- lapply(nocode,
-                                function(p) {
-                                    blockValue(p)
-                                })
-               names <- names(nocode)
-               if (length(names)) {
-                   names(params) <- gsub("op.opparams[.]?", "", names)
-               }
-               params
-           })
-}
-
 readDVI <- function(file) {
     ## Create a list that is known to be too long
     ops <- vector("list", file.size(file))
--- tests/access.R
+++ tests/access.R
@@ -6,3 +6,5 @@ dvi <- readDVI(system.file("DVI", "test-pdftex.dvi", package="dvi"))
 opCodes(dvi)
 
 opParams(dvi)
+
+opNames(opCodes(dvi))
